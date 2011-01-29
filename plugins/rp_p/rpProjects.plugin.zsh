128aeaf824035ebdab01077d065a67c2fa992878
Kevin Webster <webster.kevin@gmail.com>
2011-01-29 13:57:15 -0800

    added rp plugins

---
+++ plugins/rp_p/rpProjects.plugin.zsh
@@ -0,0 +1,13 @@
+# rpProjects function
+# Opens initializer (bootstrap) script located in the root of the project
+# The script can do anything
+#   - Load vim workspace
+#   - Open Finder to relevant files
+#   - Load dameon (such as compass watch)
+#   - Anything that zsh can accomplish
+#
+# .pinit should be the filename
+
+function p () {
+    
+}
