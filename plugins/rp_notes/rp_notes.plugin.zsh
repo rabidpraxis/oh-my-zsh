d030b9ac22fdc189daf36a2fc19ab8985473bce1
Kevin Webster <webster.kevin@gmail.com>
2011-01-29 19:54:14 -0800

    added rp plugins

---
+++ plugins/rp_notes/rp_notes.plugin.zsh
@@ -0,0 +1,14 @@
+# Ability to view notes
+# example: n vim
+# - Should be tab completable
+# - Tab completion recommentaions should be awesome
+
+fpath=($ZSH/plugins/rp_notes $fpath)
+autoload -U compinit
+compinit -i
+
+NOTES=~/Dropbox/rpLib/Notes
+
+function notes () {
+  v $NOTES/$1.mdown
+}
