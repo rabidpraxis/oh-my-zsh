336acf1561d838f914eaf9a17bb1ddce70241f66
rabidpraxis <webster.kevin@gmail.com>
2011-02-22 16:00:21 -0800

    updated startup zsh script

--- custom/startup.zsh
+++ custom/startup.zsh
@@ -4,9 +4,13 @@ function rpcmds
 
    **** $fg[yellow]rabidpraxis's Shell Commands $reset_color****
 
-   $fg_bold[red]journal $reset_color-- Journal entry for today
-         $fg_bold[red]t $reset_color-- Add global todo's
-         $fg_bold[red]n $reset_color-- Read and create notes
+      $fg_bold[yellow]wakeup $reset_color-- Wake-up reminders
+  
+     $fg_bold[red]journal $reset_color-- Journal entry for today
+           $fg_bold[red]t $reset_color-- Add global todo's
+           $fg_bold[red]n $reset_color-- Read and create notes
+
+    $fg_bold[green]contacts $reset_color-- Determine if Contacts need changing
 
 go
 }
