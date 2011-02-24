284ec124f899bff47c890a29a001337d3348474e
rabidpraxis <webster.kevin@gmail.com>
2011-02-24 09:27:36 -0800

    removed unnessecary files and added custom zsh theme

--- plugins/rp_notes/test.zsh
+++
@@ -1,9 +0,0 @@
-${
-  ${
-    (f)"$(_call_program targets $words[1] -sT $opt_args[(I)(-N|--nosearch)] ${(kv)opt_args[(I)(-f|--rakefile)]} 2>/dev/null)"
-  }
-  /(#b)rake ([^ ]##) ##\# (*)/
-  ${ ${match[1]}//:/\\: }
-  :${match[2]:l }
-}
- 
